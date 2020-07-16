5c52319d078a0b455fff91c14591f40c31409026
Koteich_dev <49362257+Koteich-dev@users.noreply.github.com>
2020-07-16 13:00:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js'),
 moment = require("moment"),
-      bot = new Discord.Client(),
+bot = new Discord.Client(),
       
       { prefix, token } = require('./config');
 bot.commands = new Discord.Collection()
@@ -11,47 +11,11 @@ require('fs').readdirSync('./commands/').filter(file => file.endsWith('.js')).fo
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 
-  bot.on("guildCreate", guild => {
-  let sicon = guild.iconURL({ dynamic: true, png: true });
-  moment.locale('ru');
-    let web = new Discord.WebhookClient('705851300796956752','8GUe3cOjxZxkHvn3NgT4rXRjeeHCynsnz8jO1IjCPuWR6bNXx1wcnrnaTJ6pD3HEll9y')
-    let nserv = new Discord.MessageEmbed()
-    .setTitle("⚙️┊Я зашел на сервера")
-    .setColor('00FF2E')
-    .setThumbnail(sicon)
-    .addField("<:ServerModule:705863798682157126>┊Название сервера: ", guild.name)
-    .addField("<:guild:705867072755859458>┊ID сервера: ", guild.id)
-    .addField("<:coder:705866387809370174>┊Владелец: ",  `${guild.owner.user.tag}`)
-    .addField("<:mobile:705866448760733948>┊ID владельца: ", guild.ownerID)
-    .addField("<:stats:705868676225826856>┊Количество серверов: ", bot.guilds.cache.size)
-    .addField("<:server:705866513021665320>┊Сервер создан ", `${moment(guild.createdAt).format('LLLL')}`)
-    .setTimestamp();
-    web.send(nserv)
-  });
-   bot.on("guildDelete", guild => {
-    let sicon = guild.iconURL({ dynamic: true, png: true });
-     moment.locale('ru');
-      let web = new Discord.WebhookClient('705851300796956752','8GUe3cOjxZxkHvn3NgT4rXRjeeHCynsnz8jO1IjCPuWR6bNXx1wcnrnaTJ6pD3HEll9y')
-      let nserv = new Discord.MessageEmbed()
-     .setColor('FF0000')
-    .setThumbnail(sicon)
-    .setTitle("⚙️┊Я вышел с сервера")
-    .addField("<:ServerModule:705863798682157126>┊Название сервера: ", guild.name)
-    .addField("<:guild:705867072755859458>┊ID сервера: ", guild.id )
-    .addField("<:coder:705866387809370174>┊Владелец: ", `${guild.owner.user.tag}`)
-    .addField("<:mobile:705866448760733948>┊ID владельца: ", guild.ownerID)
-    .addField("<:stats:705868676225826856>┊Количество серверов:", bot.guilds.cache.size)
-    .addField("<:server:705866513021665320>┊Сервер создан ", `${moment(guild.createdAt).format('LLLL')}`)
-    .setTimestamp();
-     web.send(nserv)
-    });
-
-  bot.user.setActivity('s.help',{ url : "https://www.twitch.tv/koteich_dev%", type: 'STREAMING'});
 });
 
 bot.on('message', async (message) => {
   if (message.author.bot) return;
-  if(!message.guild) return message.channel.send('Сэр, простите но я работаю только на серверах!');
+  if(!message.guild) return;
   if(message.content.startsWith(prefix)){
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
@@ -66,4 +30,4 @@ bot.on('message', async (message) => {
   }
 });
 
-bot.login("NjcyMDc2NDg4OTU4NTQxODM1.XtNYnA.lqIdbnjB36KRJXOtFhVFtL_fDBU");
+bot.login(token);
